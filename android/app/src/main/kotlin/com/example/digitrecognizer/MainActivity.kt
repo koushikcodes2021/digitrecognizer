6d3744c0f003fe5c6dc76e82cae74d58996413fa
Koushik Sahoo <85097545+koushikcodes2021@users.noreply.github.com>
2021-08-26 19:39:11 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/digitrecognizer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.digitrecognizer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:digitrecognizer/screens/draw_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'digitrecognizer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.cyanAccent,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      home: DrawScreen(),
+    );
+  }
+}
